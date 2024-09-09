d5f7e500bb8cb990d1e217e29f59015aca66af6d
unknown <SENA@ESPAPRCAJFSP006.FormacionSena.RED>
2024-09-09 14:59:49 -0500

    controladores Victor

---
+++ Backend/Apimarket/Controllers/Race.Controller.cs
@@ -0,0 +1,81 @@
+﻿using APIMARKET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIMARKET.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class RaceController : Controller
+    {
+        [HttpPost("CreateRace")]
+        public IActionResult CreateRaza(RaceModel race)
+        {
+            try
+            {
+                // Lógica para crear una nueva raza
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpGet("GetBreeds")]
+        public IActionResult GetRazas()
+        {
+            try
+            {
+                // Lógica para obtener todas las razas
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpGet("GetRace")]
+        public IActionResult GetRaza()
+        {
+            try
+            {
+                // Lógica para obtener una raza específica
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpPut("UpdateRace")]
+        public IActionResult UpdateRaza(RaceModel raza)
+        {
+            try
+            {
+                // Lógica para actualizar una raza existente
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpDelete("DeleteRace")]
+        public IActionResult DeleteRaza()
+        {
+            try
+            {
+                // Lógica para eliminar una raza
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+    }
+}
+
